4d490c7ccde1be974b5b2024823bdffbc3eeeab9
Sebastian Silva <casesime2.1@gmail.com>
2020-04-18 19:48:09 -0500

    Json implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 // rutas
 import { AppRoutingModule } from './app-routing.module';
 // componentes
@@ -21,7 +22,8 @@ import { ItemComponent } from './pages/item/item.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
